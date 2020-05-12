57980edf0b4c4f53f1f70117ca91bc8a2e91ae16
Alexandra Eyben <alexeyben@hotmail.com>
2020-05-12 13:16:15 +0200

    .

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_05_12_094916) do
-
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
@@ -19,5 +18,4 @@ ActiveRecord::Schema.define(version: 2020_05_12_094916) do
     t.datetime "updated_at", precision: 6, null: false
     t.boolean "completed", default: false
   end
-
 end
